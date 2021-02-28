43fdb20f671f69cd77efbcf1ea592d797658b3f6
goncrust <goncalonrua@gmail.com>
2021-02-28 03:24:24 +0000

    tests

---
+++ src/bot.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents(messages=True, guilds=True,
+                          reactions=True, members=True, presences=True)
+bot = commands.Bot(command_prefix='>', intents=intents)
+
+
+@bot.event
+async def on_member_join(member):
+    print(str(member) + " joined")
+
+
+@bot.event
+async def on_member_remove(member):
+    print(str(member) + " left")
+
+
+@bot.command()
+async def test(ctx):
+    await ctx.send("Rato-esquilo!")
+
+token = open("token.txt", 'r').readline()
+bot.run(token)
